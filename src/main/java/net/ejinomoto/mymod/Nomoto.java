87f2a393a7ecdc01d6bb2f3c5a04cef22027b602
Ejinomoto <ezyalden25@gmail.com>
2025-05-05 15:40:21 +0700

    tes

--- src/main/java/net/ejinomoto/mymod/Nomoto.java
+++ src/main/java/net/ejinomoto/mymod/Nomoto.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Tes
 public class Nomoto implements ModInitializer {
 	public static final String MOD_ID = "nomoto";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
